c55e50cbaf07e8851bf9389ec51bff8e58dd40eb
Aron <aron@mywheels.nl>
2018-10-05 17:27:08 +0200

    Last fixes

--- src/common/directives/vouchers/vouchersDirective.js
+++ src/common/directives/vouchers/vouchersDirective.js
@@ -75,6 +75,7 @@ angular.module('vouchersDirective', [])
           }
           booking.details = bookingObject;
           $scope.booking = booking;
+          $scope.blockedUntil = moment(booking.createdAt).add('minutes', 15);
           $scope.holidaytrip = moment(booking.createdAt).add('days', 7).isBefore(moment(booking.cancelAfter));
           $scope.booking.details.extra_drivers_price = $scope.extraDrivers.check ? ($scope.extraDrivers.drivers.length + $scope.booking.details.drivers_count) * $scope.extraDrivers.price : 0;
 
