4c22e20cdd9add8366d652112642c2817c8ec936
Kelley van Evert <hello@kelleyvanevert.nl>
2018-11-21 15:55:10 +0100

    fixed a stupid bug where the return time extension buttons for the timeframe picker would not affect the date, although the time went past 00:00

--- src/common/directives/timeframePicker/timeframePickerDirective.js
+++ src/common/directives/timeframePicker/timeframePickerDirective.js
@@ -153,14 +153,20 @@ angular.module('timeframePickerDirective', [])
           returnTime: moment($scope.returnTime, timeConfig.format), // only used for time part (!)
         };
 
-        if (tf.returnTime.isValid()) {
-          $scope.returnTime = tf.returnTime.add(numHours, 'hours').format(timeConfig.format);
+        if (tf.returnDate.isValid() && tf.returnTime.isValid()) {
+          // consolidate
+          tf.return = moment($scope.returnDate + ' ' + $scope.returnTime, dateConfig.format + ' '+ timeConfig.format);
+
+          // add hour extension
+          tf.return = tf.return.add(numHours, 'hours');
+
+          // write back
+          $scope.returnDate = tf.return.format(dateConfig.format);
+          $scope.returnTime = tf.return.format(timeConfig.format);
+          $scope.form.returnDate.$setTouched(true); // ??
           $scope.form.returnTime.$setTouched(true); // ??
 
-          if (tf.returnDate.isValid()) {
-            tf.return = moment($scope.returnDate + ' ' + $scope.returnTime, dateConfig.format + ' '+ timeConfig.format);
-            $scope.returnDateTime = tf.return.format(API_DATE_FORMAT);
-          }
+          $scope.returnDateTime = tf.return.format(API_DATE_FORMAT);
         }
       }
 
