d7ec5d87f75949486b572a0acb1fe99f36ca2560
Kelley van Evert <hello@kelleyvanevert.nl>
2018-11-21 15:44:16 +0100

    top of card car pricing component redesign
    Conflicts (resolved):
    	src/app/app.js

---
+++ src/common/directives/resourcePricing/resourcePricingDirective.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * Kelley van Evert, 21 nov 2018
+ * API:
+ *   <resource-pricing
+ *      resource     (=)  :: resource object
+ *   />
+ */
+angular.module('resourcePricingDirective', [])
+
+.directive('resourcePricing', function resourcePricing ($log, mobileDetectService) {
+
+  // Configuration, constants, helpers
+  // =====
+
+  var mobile = (mobileDetectService.phone() || mobileDetectService.mobile() || mobileDetectService.tablet());
+
+
+  // The directive
+  // =====
+
+  return {
+    restrict: 'E',
+    scope: {
+      resource: '=',
+    },
+    templateUrl: 'directives/resourcePricing/resourcePricing.tpl.html',
+    replace: true,
+    controller: function resourcePricingController ($scope, $element, $log) {
+
+      $scope.showPricePerHour = false;
+
+      if (!$scope.resource.price.kmFree) {
+        $scope.kmPrice = parseFloat($scope.resource.price.kilometerRate) + parseFloat($scope.resource.price.fuelPerKilometer);
+      }
+    },
+  };
+
+});
