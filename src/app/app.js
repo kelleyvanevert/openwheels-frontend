3e55358d0d35fc70cf0f442ec8146c7093494d6f
Kelley van Evert <hello@kelleyvanevert.nl>
2019-02-08 16:05:51 +0100

    recovered (1)(2)(3)(4) steps in car registration process

--- src/app/app.js
+++ src/app/app.js
@@ -445,6 +445,7 @@ angular.module('openwheels', [
       $state.includes('subscribe') ||
       $state.includes('invite') ||
       $state.includes('member') ||
+      $state.includes('owm.resource.create') ||
       $state.includes('owm.message') ||
       $state.includes('owm.trips') ||
       $state.includes('owm.finance.vouchers') ||
