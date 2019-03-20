55ef1a04d2b121a1f470eaba613d1db616b6b955
Kelley van Evert <hello@kelleyvanevert.nl>
2019-03-20 10:23:46 +0100

    beheerderspagina content

--- src/app/pages/beheerders/BeheerdersPaginaController.js
+++ src/app/pages/beheerders/BeheerdersPaginaController.js
@@ -73,35 +73,15 @@ angular.module('owmlanding.beheerders', ['slick'])
   // Veelgestelde vragen
   $scope.FAQ = [
     {
-      question: 'Hoeveel kilometer mag ik per jaar rijden?',
-      answer: 'De leaseprijs is inclusief 12.000 kilometer per jaar. De kilometerbundel is zowel voor je privé kilometers, als voor de kilometers van huurders in jouw leaseauto. Boven dit aantal kilometers per jaar is de meerprijs per kilometer 8 cent.',
+      question: 'Wat zijn de taken van een beheerder?',
+      answer_html: '<p>Een beheerder let erop dat diens auto in topconditie blijft. Concreet houdt dit in:</p><ul><li>nieuwe leden uitleg geven;</li><li>contact met gebruikers in de buurt onderhouden;</li><li>de reservesleutel beheren en bij nood beschikbaar stellen;</li></ul><p>Jaarlijks onderhoud wordt door de garage gedaan. Als beheerder maak je hier een afspraak voor.</p>',
     }, {
-      question: 'Wat is de looptijd?',
-      answer: 'De looptijd is 48 maanden, een kortere looptijd is niet mogelijk. Wil je het contract eerder beëindigen? Dat kan door het contract eenmalig af te kopen voor 1.000 euro.',
-    }, {
-      question: 'Krijg ik MyWheels bestickering op mijn auto?',
-      answer: 'Ja, we plakken MyWheels stickers op de auto. De buren zien dan dat ze jouw auto kunnen huren. Wil je geen bestickering? Dan betaal je 40 euro per maand extra.',
-    }, {
-      question: 'Wat is inbegrepen in de leaseprijs?',
-      answer: 'De leaseprijs is inclusief: all-risk verzekering, schade verzekering inzittenden, wegenbelasting, afschrijving, reparatie, onderhoud, banden, ruitreparatie, pechhulp in binnen- en buitenland, vervangend vervoer bij schade en MyWheels Open technologie. Enkel de brandstof, verkeersboetes en de wasstraat vallen buiten de leaseprijs.',
-    }, {
-      question: 'Hoe verhuur ik mijn leaseauto?',
-      answer: 'Verhuren is heel makkelijk: nieuwe huurverzoeken worden automatisch geaccepteerd. Door de MyWheels Open technologie opent een huurder jouw auto met zijn of haar smartphone. Het enige dat jij hoeft te doen, is het bijhouden van de kalender van je auto.',
-    }, {
-      question: 'Hoe veel dagen moet mijn auto beschikbaar zijn?',
-      answer: 'We verwachten dat jouw auto minimaal 10 dagen per beschikbaar is voor autodelen, waarvan 4 dagen in het weekeinde.',
-    }, {
-      question: 'Waarom is de leaseprijs zo voordelig?',
-      answer: 'De leaseprijs is voordeliger dan bij andere aanbieders omdat maandelijks de eerste 100 euro aan verhuuropbrengst naar de leasemaatschappij gaat. Verdien je meer? Dan ontvang jij de extra verhuuropbrengst.',
-    }, {
-      question: 'Hoe is de auto verzekerd tijdens verhuur?',
-      answer: 'De auto is per verhuring aanvullend all-risk verzekerd met de ritverzekering van Centraal Beheer via het MyWheels platform.',
-    }, {
-      question: 'Hoe worden huurders gecontroleerd?',
-      answer: 'Een huurder moet in bezit zijn van een geldig rijbewijs en een geverifieerde bankrekening hebben. Daarnaast doen wij extra controles ter beoordeling van de betrouwbaarheid van de huurder.',
-    }, {
-      question: 'In welke steden geldt dit aanbod?',
-      answer: 'Dit private lease aanbod is geldig voor inwoners van de volgende gebieden: Amersfoort, Amstelveen, Amsterdam, Arnhem, Delft, Den Haag, Deventer, Eindhoven, Groningen, Haarlem, Hilversum, Leiden, Leeuwarden, Nijmegen, Rijswijk, Rotterdam, Utrecht, Wageningen, Zeist en Zwolle.',
+      question: 'Is er een vergoeding voor beheerderschap?',
+      answer: 'Ja, al bedankje krijgen MyWheels beheerders maandelijks een vrijwilligersvergoeding in de vorm van 75 vrije kilometers.',
+    },
+    {
+      question: 'Wat als er geen MyWheels Open auto in mijn buurt staat?',
+      answer_html: '<p>Staat er nog geen auto met MyWheels Open bij jou in de buurt? Neem dan <a href="https://mywheels.nl/autodelen/community/contact/">contact</a> met ons op om de mogelijkheden te bespreken.</p>',
     },
   ];
 
